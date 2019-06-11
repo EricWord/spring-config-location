7dc8e49566abe4267840113349686aef181a9eef
EricWord <2635863525@qq.com>
2019-06-11 13:15:08 +0800

    Initial commit

---
+++ src/test/java/cn/cncoders/springconfiglocation/SpringConfigLocationApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cncoders.springconfiglocation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringConfigLocationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
